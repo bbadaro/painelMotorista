083f488d61573eb822463c78c1b259c979246c17
Rodrigo Badaro <rbadaro92@gmail.com>
2019-01-29 23:31:46 -0200

    first commit

---
+++ src/app/tabs/tabs.router.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/slider', pathMatch: 'full' },
+  {
+    path: 'tabs', component: TabsPage, children: [
+      { path: 'tab1', loadChildren: '../tab1/tab1.module#Tab1PageModule' },
+      { path: 'tab2', loadChildren: '../tab2/tab2.module#Tab2PageModule' },
+      { path: 'tab3', loadChildren: '../tab3/tab3.module#Tab3PageModule' },
+    ]
+  },  
+
+
+];
+
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TabsPageRoutingModule { }
